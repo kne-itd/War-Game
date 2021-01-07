92acaaafd71deffaf5576061d6a900ecd07afb51
kaj <kne@edu.sde.dk>
2021-01-07 09:20:45 +0100

    Added comment

--- War/Extensions.cs
+++ War/Extensions.cs
@@ -6,7 +6,12 @@ namespace War
 {
     public static class Extensions
     {
-        public static void Enqueue(this Queue<Card> cards, Queue<Card> newCards)
+        //The first parameter of the method specifies the type that the method operates on;
+        //it must be preceded with the this modifier.
+        //In the calling code, add a using directive to specify the namespace
+        //that contains the extension method class. Call the methods as if they were instance methods on the type.
+
+       public static void Enqueue(this Queue<Card> cards, Queue<Card> newCards)
         {
             foreach (var card in newCards)
             {
